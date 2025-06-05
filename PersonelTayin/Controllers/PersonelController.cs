d6ca92076282d3a9de11a2eaccbc9b3cadfe63c3
Efe Özgür <efecuneytozgur@gmail.com>
2025-06-05 16:07:34 +0300

    Update PersonelController.cs

--- PersonelTayin/Controllers/PersonelController.cs
+++ PersonelTayin/Controllers/PersonelController.cs
@@ -45,20 +45,6 @@ namespace PersonelTayin.Controllers
             return RedirectToAction("Index","Home");
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create(IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
 
         public ActionResult Edit(int id)
         {
